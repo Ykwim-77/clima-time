c154e062363dbcb2d4a3555e166003ef35bb084a
Christian <christian.darosa0106@gmail.com>
2025-09-25 19:36:11 -0300

    rafa

--- trabalho-clima/src/app/pages/clima/clima.component.ts
+++ trabalho-clima/src/app/pages/clima/clima.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-clima',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClimaComponent implements OnInit {
 
+  navegaodr = inject(Router);
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  navegarParaTelaTelaPesquisa() {
+    this.navegaodr.navigate(['pesquisa']);
+  }
 }
