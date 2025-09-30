3578bbe353a751ee53d77212eef0fa1f94b9b698
Christian <christian.darosa0106@gmail.com>
2025-09-30 19:14:47 -0300

    desculpa por tudo professor

--- trabalho-clima/src/app/pages/clima/clima.component.ts
+++ trabalho-clima/src/app/pages/clima/clima.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OpenWeatherService } from '../service/open-weather.service';
+import { weatherResponse } from '../models/weather-responde.model';
 
 @Component({
   selector: 'app-clima',
@@ -11,6 +12,7 @@ export class ClimaComponent implements OnInit {
 
   navegaodr = inject(Router);
   openWeatherService = inject(OpenWeatherService);
+  dadosClima: weatherResponse | null = null;
   constructor() { }
 
   ngOnInit() {
