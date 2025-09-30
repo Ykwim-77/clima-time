c9a040ba096f3217b5b394e6bf846271d0e1e07c
Christian <christian.darosa0106@gmail.com>
2025-09-30 19:21:48 -0300

    eu to ficando louco

--- trabalho-clima/src/app/pages/service/open-weather.service.ts
+++ trabalho-clima/src/app/pages/service/open-weather.service.ts
@@ -1,19 +1,27 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { weatherResponse } from "../models/weather-responde.model";
 
 @Injectable({
     providedIn: "root",
 })
 export class OpenWeatherService {
-    apiKey = "df7e579b75bed9feef7a6b65c50974e7";
-    apiUrl = "https://api.openweathermap.org/data/2.5/weather";
-    constructor() {}
+    private apiKey = "df7e579b75bed9feef7a6b65c50974e7";
+    private apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+    private http = inject(HttpClient);
 
-    buscarInfoClimaCidadeAtual (){
+    constructor(){
+
+    }
+
+    buscarInfoClimaCidadeAtual (): Observable<weatherResponse> {
 
 
 
-        const url = this.apiUrl + '?q=Dois Vizinhos&appid=' + this.apiKey + '&lang=pt_br&units=metric';
+        const url = 
+            this.apiUrl + '?q=Dois Vizinhos&appid=' + this.apiKey + '&lang=pt_br&units=metric';
     
-        console.log(url);
+        return this.http.get<weatherResponse>(url);
     }
 }
